75df11609dc0dddb339c53a7add5feed5b59cc6c
k4rtikay <katt1bh4i@gmail.com>
2025-06-30 22:18:08 +0530

    Basic structure and styling of landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,5 +1,21 @@
+import './Landing.css'
+
 export function Landing(){
     return(
-        <h1>Welcome</h1>
+        <div className="landingPageContent">
+            <div className="mainLandingContent">
+                <h1>PokéBook</h1>
+                <ul>
+                    <li>-Get <span>Pokédex</span> data</li>
+                    <li>-Get the <span>color palette</span> from your favorite pocket monsters</li>
+                    <li>-<span>Build a team</span> around your favorite mons.</li>
+                </ul>
+                <button className="loginOptions" style={{backgroundColor:'var(--button-secondary-bg)'}}>Sign in</button>
+                <button className="loginOptions" style={{backgroundColor:'var(--button-secondary-bg)'}}>Sign in</button>
+                <button className="loginOptions" style={{backgroundColor:'var(--button-primary-bg)'}}>Guest &gt;&gt;</button>
+                <p>Not a registered user? <u>Sign Up</u>!</p>
+            </div>
+            <footer>Made with ❤️ by <a href="https://github.com/k4rtikay" class="underline">Kartik</a></footer>
+        </div>
     )
 }
