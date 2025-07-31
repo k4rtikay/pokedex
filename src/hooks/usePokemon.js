fa710acce9b4277bad5f229f4b84f15884d4cbb7
k4rtikay <katt1bh4i@gmail.com>
2025-07-31 07:22:41 +0000

    start working on a context for streamlined access to data from pokeApi

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -1,44 +1,66 @@
 import { useState, useEffect } from "react";
 
+function useCache(cacheKey){
+    const getCache = () => JSON.parse(localStorage.getItem(cacheKey) || {});
+    const setCache = (data) => localStorage.setItem(cacheKey, JSON.stringify(data))
+
+    return( getCache,setCache)
+}
+
 export function usePokemon(pokemon){
     const [data,setData]=useState(null);
     const [loading, setLoading]=useState(false);
     const [description, setDescription] = useState('')
+    const { getCache: getPokedexCache, setCache: setPokedexCache } = useCache('pokedex')
+    const { getCache: getDescCache, setCache: setDescCache } = useCache('descriptionCache')
+
 
     useEffect(()=>{
         if(!pokemon) return
 
-        let cache={}
-        
-        if(localStorage.getItem('pokedex')){
-            cache = JSON.parse(localStorage.getItem('pokedex'))
-        }
-
-        if(selectedPokemon in cache){
-            setData(cache[pokemon])
-            console.log('pokemon found in cache')
-            return
-        }
-
         async function fetchPokemon(){
             setLoading(true)
             try{
-                let baseUrl = 'https://pokeapi.co/api/v2/'
-                let suffix = `pokemon/${getPokedexNumber(selectedPokemon)}`
-                let res = await fetch(baseUrl+suffix);
-                let resData = await res.json();
-                console.log("fetched pokemon")
-                setData(resData)
-                console.log(resData)
-                cache[pokemon]=resData
-                localStorage.setItem('pokedex', JSON.stringify(cache))
+
+                const pokedexCache = getPokedexCache()
+                if(pokedexCache[pokemon]){
+                    setData([pokedexCache[pokemon]])
+                }else{
+                    let baseUrl = 'https://pokeapi.co/api/v2/'
+                    let suffix = `pokemon/${getPokedexNumber(selectedPokemon)}`
+                    let res = await fetch(baseUrl+suffix);
+                    let resData = await res.json();
+                    console.log("fetched pokemon")
+                    setData(resData)
+                    console.log(resData)
+                    pokedexCache[pokemon]=resData
+                    setPokedexCache(pokedexCache)
+                }
+
+                //now fetch the description using the received pokemon data
+                const speciesUrl = data?.species?.url;
+                if(speciesUrl){
+                    const descCache = getDescCache()
+
+                    if(descCache[data?.species?.name]){
+                        setDescription(descCache[data?.species?.name])
+                    }else{
+                        const descRes = await fetch(speciesUrl)
+                        const descData = await descRes.json()
+                        const enDescription  = descData?.flavor_text_entries.find(entry=>(entry.version.name==='firered' && entry.language.name==='en'))?.flavor_text
+                        setDescription(enDescription)
+                        descCache[data?.species?.name] = enDescription
+                        setDescCache(descCache)
+                    }
+                }
+                
             }catch(err){
-                console.log(err)
+                console.error(err)
             }finally{
                 setLoading(false)
             }
         }
     },[])
 
-    return { data, description, loading, error}
+    return { data, description, loading}
 }
