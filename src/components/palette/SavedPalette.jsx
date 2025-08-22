ecb2e786d18886d622858d4612771ec2e4ea0007
k4rtikay <katt1bh4i@gmail.com>
2025-08-22 04:17:16 +0000

    implement a modal for confirmation of delete action

--- src/components/palette/SavedPalette.jsx
+++ src/components/palette/SavedPalette.jsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 import { useDatabase } from '../../Context/DatabaseContext';
 import './SavedPalette.css'
+import { Modal } from '../Modal/Modal';
 
 export default function SavedPalette(props){
 
     const { id,palette, name } = props;
     const { deletePalette } = useDatabase()
     const uniqueSprites = [...new Set(palette.map(p => p.sourceSprite))];
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
 
     console.log(id)
 
     return(
         <div>
             <div className="paletteCard">
+                <Modal isModalOpen={isDeleteModalOpen} onClose={()=>{setIsDeleteModalOpen(false)}}>
+                    <p>Are you sure you want to delete this palette?</p>
+                    <button>Cancel</button>
+                    <button onClick={()=>{deletePalette(id)}}>Delete</button>
+                </Modal>
                 <div className="palette-color-stripes">
                     {palette.map((paletteObj,paletteObjIndex)=>{
                         const stripeBg = `rgb(${paletteObj.color.join(', ')})`
@@ -26,7 +34,7 @@ export default function SavedPalette(props){
                         <button><span className="fa-solid fa-file-export"></span></button>
                         <button><span className="fa-solid fa-pen-to-square"></span></button>
                         <button
-                        onClick={()=>{deletePalette(id)}}><span className="fa-regular fa-trash-can"></span></button>
+                        onClick={()=>{setIsDeleteModalOpen(true)}}><span className="fa-regular fa-trash-can"></span></button>
                     </div>
                     <div className="palette-sprites-container">
                     {
