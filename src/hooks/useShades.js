8ff753b04bc84629b4b1b8f13f97a8ee1a00b216
k4rtikay <katt1bh4i@gmail.com>
2025-08-23 07:17:59 +0000

    create custom hook for shades extraction from the image colors using tinycolor2

---
+++ src/hooks/useShades.js
@@ -0,0 +1,40 @@
+import tinycolor from "tinycolor2";
+
+const shadesGenerator = (inputColor) => {
+  const baseColor = tinycolor(inputColor)
+  const { h, s, l } = baseColor.toHsl()
+  const totalShades = 20
+  const shadesArray = []
+
+  //Generate the full gradient
+  for (let i = 0; i < totalShades; i++) {
+    const lightness = 1 - (i / (totalShades - 1))
+    const newHslColor = { h, s, l: lightness }
+    shadesArray.push({
+      color: tinycolor(newHslColor).toRgbString(),
+      isOriginal: false
+    })
+  }
+
+  
+  let closestIndex = 0;
+  let minDiff = 1;
+  for (let i = 0; i < shadesArray.length; i++) {
+    const currentLightness = 1 - (i / (totalShades - 1))
+    const diff = Math.abs(currentLightness - l)
+    if (diff < minDiff) {
+      minDiff = diff
+      closestIndex = i
+    }
+  }
+
+  //Replace the closest approximation with the true original color
+  shadesArray[closestIndex] = {
+    color: baseColor.toRgbString(),
+    isOriginal: true
+  }
+
+  return shadesArray
+}
+
+export default shadesGenerator
