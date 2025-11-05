5d2a8892480186904a368d3c927fdbeb62cf50c2
k4rtikay <katt1bh4i@gmail.com>
2025-11-05 14:21:37 +0000

    feat: add modal for the about option in the menu

--- src/components/PopupMenu/PopupMenu.jsx
+++ src/components/PopupMenu/PopupMenu.jsx
@@ -9,9 +9,10 @@ import { usePokedex } from '../../Context/PokedexContext'
 
 
 export default function PopupMenu({ isMenuActive, setIsMenuActive, triggerRef }){
-    // const [isMenuActive, setIsMenuActive] = useState(false)
     const [isAuthOpen, setIsAuthOpen] = useState(false)
+    const [isAboutOpen, setIsAboutOpen] = useState(false);
     const [menuPosition, setMenuPosition] = useState({top: 0, left: 0, right: 0, iconSize: 0})
+
     const { globalUser,logout } = useAuth()
     const navigate = useNavigate();
     const { isDesktop, isDarkMode, setIsDarkMode } = usePokedex()
@@ -123,63 +124,92 @@ export default function PopupMenu({ isMenuActive, setIsMenuActive, triggerRef })
                 <Auth></Auth>
             </Modal>
 
-                <AnimatePresence>
+            <Modal isModalOpen={isAboutOpen} onClose={() => { setIsAboutOpen(false) }}>
+                <div className="about-modal-content">
+                    <h2>About HueDex</h2>
+                    <p>
+                        HueDex is a color palette generator inspired by the world of Pokémon.
+                        It's a personal project built with React, Framer Motion, and Firebase to explore design, animation, and web development.
+                    </p>
 
-                    <button className="menu-trigger"
-                    onClick={()=>{setIsMenuActive(true)}}><span className="h-icon material-symbols-rounded">account_circle</span></button>
-                    { isMenuActive &&
-                        (
-                                <motion.div
-                                key={'username'}
-                                variants={variants}
-                                initial= {'closed'}
-                                className='menu'
-                                animate= {'open'} 
-                                exit={'closed'}
-                                style={{
-                                    transformOrigin: isDesktop ? 'top right' : 'top left'
-                                }}
-                                >
-                                    <div className={"menu-top "}>
-                                            <motion.div
-                                            variants={nameVariants}
-                                            initial='closed'
-                                            aria-hidden={!isMenuActive}
-                                            animate={'open'}
-                                            exit={'closed'}
-                                            key={'name'}
-                                            style={{}}>{(globalUser?globalUser.displayName:'Guest')}</motion.div>       
-                                    </div>
+                    <p>
+                        You can explore by selecting a Pokémon from the list to see its color palette and press Spacebar on desktop or use the 'Generate' button to discover new ones on mobile. 
+                        Don't forget to save your favorites to your personal library! 
+                        All Pokémon data and sprites are gratefully provided by the <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">PokéAPI</a>.
+                    </p>
+                    <a
+                        href="https://github.com/k4rtikay/pokedex" //Project URL
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="about-github-link"
+                    >
+                        See Code on GitHub
+                    </a>
+                </div>
+            </Modal>
 
-                                        <nav className="menu-nav">
-                                            <motion.button variants={navItemVariants} whileHover={{opacity:1}}>About</motion.button>
+            <AnimatePresence>
 
-                                            <motion.a variants={navItemVariants} href='https://github.com/k4rtikay/pokedex/issues' target='_blank' rel='noopener noreferrer' whileHover={{opacity:1}}>Feedback</motion.a>
+                <button className="menu-trigger"
+                onClick={()=>{setIsMenuActive(true)}}><span className="h-icon material-symbols-rounded">account_circle</span></button>
+                { isMenuActive &&
+                    (
+                            <motion.div
+                            key={'username'}
+                            variants={variants}
+                            initial= {'closed'}
+                            className='menu'
+                            animate= {'open'} 
+                            exit={'closed'}
+                            style={{
+                                transformOrigin: isDesktop ? 'top right' : 'top left'
+                            }}
+                            >
+                                <div className={"menu-top "}>
+                                        <motion.div
+                                        variants={nameVariants}
+                                        initial='closed'
+                                        aria-hidden={!isMenuActive}
+                                        animate={'open'}
+                                        exit={'closed'}
+                                        key={'name'}
+                                        style={{}}>{(globalUser?globalUser.displayName:'Guest')}</motion.div>       
+                                </div>
 
-                                            <motion.button variants={navItemVariants}
-                                            whileHover={{opacity:1}}
-                                            onClick={()=>{setIsDarkMode(!isDarkMode)}}>Toggle Theme</motion.button>
+                                    <nav className="menu-nav">
+                                        <motion.button variants={navItemVariants} whileHover={{opacity:1}}
+                                        onClick={() => {
+                                        setIsAboutOpen(true);
+                                        setIsMenuActive(false);
+                                        }}>About</motion.button>
 
-                                            <motion.button variants={navItemVariants}
-                                            whileHover={{opacity:1}}
-                                            className={globalUser && 'menu-signout'}
-                                            onClick={async ()=>{
-                                                if(globalUser){
-                                                    console.log('logging out..')
-                                                    await logout()
-                                                    console.log('navigating..')
-                                                    navigate('/')
-                                                }else{
-                                                    setIsAuthOpen(true)
-                                                    setIsMenuActive(false)
-                                            }}}>{globalUser?'Sign Out':'Sign In'}</motion.button>
-                                        </nav>
-                                        
-                                        <motion.footer variants={navItemVariants}>Made with ❤️ by <a href='https://github.com/k4rtikay' target="_blank" rel='noopener noreferrer'>k4rtikay</a> </motion.footer>
-                                </motion.div>
-                        )
-                    }
-                </AnimatePresence>
+                                        <motion.a variants={navItemVariants} href='https://github.com/k4rtikay/pokedex/issues' target='_blank' rel='noopener noreferrer' whileHover={{opacity:1}}>Feedback</motion.a>
+
+                                        <motion.button variants={navItemVariants}
+                                        whileHover={{opacity:1}}
+                                        onClick={()=>{setIsDarkMode(!isDarkMode)}}>Toggle Theme</motion.button>
+
+                                        <motion.button variants={navItemVariants}
+                                        whileHover={{opacity:1}}
+                                        className={globalUser && 'menu-signout'}
+                                        onClick={async ()=>{
+                                            if(globalUser){
+                                                console.log('logging out..')
+                                                await logout()
+                                                console.log('navigating..')
+                                                navigate('/')
+                                            }else{
+                                                setIsAuthOpen(true)
+                                                setIsMenuActive(false)
+                                        }}}>{globalUser?'Sign Out':'Sign In'}</motion.button>
+                                    </nav>
+                                    
+                                    <motion.footer variants={navItemVariants}>Made with ❤️ by <a href='https://github.com/k4rtikay' target="_blank" rel='noopener noreferrer'>k4rtikay</a> </motion.footer>
+                            </motion.div>
+                    )
+                }
+            </AnimatePresence>
+
         </div>
     )
 }
