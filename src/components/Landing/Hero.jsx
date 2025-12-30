224df40da80320a4b59c8126be96dbf17802b691
k4rtikay <katt1bh4i@gmail.com>
2025-12-30 15:04:50 +0000

    refactor: redesign hero text of the portfolio

---
+++ src/components/Landing/Hero.jsx
@@ -0,0 +1,13 @@
+import "./hero.scss";
+
+export default function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <h1>Iconic <span>Pokémon</span> Colors, <br/> Ready for Real Projects!</h1>
+                <p>Design faster with color palettes inspired by Pokémon—crafted for designers and developers who want nostalgia and precision. Generate, lock, tweak, and export palettes in seconds.</p>
+                <button>Start Creating</button>
+            </div>
+        </div>
+    )
+}
