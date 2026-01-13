2d5292aed21afe7ada115956b9d1d062cdbb343d
k4rtikay <katt1bh4i@gmail.com>
2026-01-13 12:52:34 +0000

    feat: add seo to the landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -10,48 +10,50 @@ import { Footer } from "./Footer";
 
 export default function Landing() {
   const [isAuthOpen, setIsAuthOpen] = useState(false);
- 
+
 
   return (
-    <motion.div
-      initial={{
-        transition: {
-          type: "spring",
-          stiffness: 120,
-          damping: 25,
-        },
-      }}
-      exit={{
-        opacity: 0.3,
-        filter: "blur(5px)",
-      }}
-    >
-      <div className="lp-container">
-        <header className="lp-header">
-          <h1>Huedex</h1>
-          <button onClick={() => setIsAuthOpen(true)}>Sign In</button>
-        </header>
-        <section>
-          <Hero />
-        </section>
+    <>
+      <title>HueDex - Pokemon Color Palettes</title>
+      <meta name="description" content="Generate stunning color palettes from pokemon sprites and export them for use in your own css." />
 
-        <section>
-          <Features />
-        </section>
-
-        <section>
-          <Footer />
-        </section>
-      </div>
-
-      <Modal
-        isModalOpen={isAuthOpen}
-        onClose={() => {
-          setIsAuthOpen(false);
+      <motion.div
+        initial={{
+          transition: {
+            type: "spring",
+            stiffness: 120,
+            damping: 25,
+          },
+        }}
+        exit={{
+          opacity: 0.3,
+          filter: "blur(5px)",
         }}
       >
-        <Auth></Auth>
-      </Modal>
-    </motion.div>
+        <div className="lp-container">
+          <header className="lp-header">
+            <h1>Huedex</h1>
+            <button onClick={() => setIsAuthOpen(true)}>Sign In</button>
+          </header>
+          <section>
+            <Hero />
+          </section>
+          <section>
+            <Features />
+          </section>
+          <section>
+            <Footer />
+          </section>
+        </div>
+        <Modal
+          isModalOpen={isAuthOpen}
+          onClose={() => {
+            setIsAuthOpen(false);
+          }}
+        >
+          <Auth></Auth>
+        </Modal>
+      </motion.div>
+    </>
   );
 }
