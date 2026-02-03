6b95ab31e9321da354ca2a3c6c43c044e8b26685
k4rtikay <katt1bh4i@gmail.com>
2026-02-03 12:45:02 +0000

    feat: add enter animations

--- src/components/Landing/Features.jsx
+++ src/components/Landing/Features.jsx
@@ -1,5 +1,6 @@
 import "./features.scss";
 import { LockSkeleton, SaveSkeleton, ExportSkeleton, ShinySkeleton } from "./Skeleton";
+import { motion } from "framer-motion";
 
 
 const features = [
@@ -26,17 +27,72 @@ const features = [
 
 ]
 
+const parentVariants = {
+    hidden: {
+        // opacity: 0,
+    },
+    visible: {
+        // opacity: 1,
+        transition: {
+            staggerChildren: 0.2,
+        }
+    }
+}
+
+const childVariants = {
+    hidden: {
+        opacity: 0,
+        y: 20
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.5,
+            type: "spring",
+            stiffness: 100,
+            damping: 20
+        }
+    }
+}
+
+const skeletonVariants = {
+    hidden: { 
+        scale: 0.95 
+    },
+    visible: { 
+        scale: 1,
+        transition: { 
+            delay: 0.5, 
+            duration: 0.2 
+        }
+    }
+}
+
 
 export function Features() {
     return (
-        <div className="features">
+        <motion.div className="features"
+            variants={parentVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.25 }}
+        >
             {features.map((feature, index) => (
-                <div key={index} className="feature">
-                    <div className="feature-skeleton">{feature.skeleton}</div>
+                <motion.div
+                    key={index}
+                    className="feature"
+                    variants={childVariants}
+                >
+                    <motion.div className="feature-skeleton" 
+                    variants={skeletonVariants}
+                    >
+                        {feature.skeleton}
+                    </motion.div>
                     <h3>{feature.heading}</h3>
                     <p>{feature.description}</p>
-                </div>
+                </motion.div>
             ))}
-        </div>
+        </motion.div>
     )
 }
