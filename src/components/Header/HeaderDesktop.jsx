ce92e5c9b821314166f94d3ceed07e7990e09e44
k4rtikay <katt1bh4i@gmail.com>
2026-04-04 15:29:20 +0000

    style: library button has a background and active animation

--- src/components/Header/HeaderDesktop.jsx
+++ src/components/Header/HeaderDesktop.jsx
@@ -20,7 +20,7 @@ export default function HeaderDesktop() {
               setIsLibraryOpen(!islibraryOpen);
             }}
           >
-            Library
+            My Palettes
           </button>
           <span>
             <PopupMenu
