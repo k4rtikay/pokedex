d2eca74eef7b811356810b6c022270b4c977e820
k4rtikay <katt1bh4i@gmail.com>
2026-04-05 18:04:11 +0000

    feat: landing page cards interaction

--- src/components/Landing/Hero.jsx
+++ src/components/Landing/Hero.jsx
@@ -2,39 +2,6 @@ import "./hero.scss";
 import { useNavigate } from "react-router-dom";
 import { PaletteIllustration } from "./PaletteIllustration";
 
-const samplePalettes = [
-    {
-        name: "Pikachu",
-        id: "#025",
-        spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
-        colors: ["#F8D030", "#A8A878", "#f86058ff", "#705848", "#a24b08", "#fcc840"]
-    },
-    {
-        name: "Charizard",
-        id: "#006",
-        spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
-        colors: ["#082933", "#cc5444", "#ec963b", "#84341c", "#247494", "#cccccc"]
-    },
-    {
-        name: "Mewtwo",
-        id: "#150",
-        spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png",
-        colors: ["#A040A0", "#F8F8F8", "#705898", "#A8A8C0", "#483D8B", "#b2b2f7ff"]
-    },
-    {
-        name: "Typhlosion",
-        id: "#157",
-        spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/157.png",
-        colors: ["#34405a", "#f3e38d", "#e82804", "#fcd004", "#5c7cbc", "#a48c4c"]
-    },
-    {
-        name: "Lapras",
-        id: "#131",
-        spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/131.png",
-        colors: ["#6890F0", "#98D8D8", "#A8A8C0", "#705848", "#1c4454", "#dcc484"]
-    }
-];
-
 export default function Hero() {
     const navigate = useNavigate();
 
@@ -50,9 +17,7 @@ export default function Hero() {
                 >Start Creating</button>
             </div>
             <div className="hero-illustration">
-                <PaletteIllustration
-                    palettes={samplePalettes}
-                />
+                <PaletteIllustration/>
             </div>
         </div>
     )
